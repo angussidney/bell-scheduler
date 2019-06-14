2c5f3544defb232f5992e5d86862b259047012c7
angussidney <4848802+angussidney@users.noreply.github.com>
2019-06-14 13:32:55 +1000

    Table row selection now works correctly

--- bell-server/static/main.js
+++ bell-server/static/main.js
@@ -1,27 +1,31 @@
-let all_bells = [],
-    selected_bells = [];
+// TODO: Use actual date obj instead of string
 
-function Bell(time) { // todo keep going
-    this.time = row.getElementsByTagName("td")[0].innerHTML;
-    this.name = row.getElementsByTagName("td")[1].innerHTML;
-    this.filepath = row.getElementsByTagName("td")[2].innerHTML;
+let all_bells = [], // List of bell objects
+    selected_bells = []; // List of names
+
+function Bell(time, name, filepath) {
+    this.time = time;
+    this.name = name;
+    this.filepath = filepath;
     this.renderHTML = function () {
         return "<tr><td>" + this.time + "</td><td>" + this.name + "</td><td>" + this.filepath + "</td></tr>"
     }
 }
 
-function create_bell_from_html(row) {
-    let time = row.getElementsByTagName("td")[0].innerHTML;
-    let name = row.getElementsByTagName("td")[1].innerHTML;
-    let filepath = row.getElementsByTagName("td")[2].innerHTML;
-    return new Bell(time, name, filepath);
+function get_bell(bell_name) {
+    for (let bell of all_bells) {
+        if (bell.name === bell_name) {
+            return bell
+        }
+    }
+    return null;
 }
 
-let bell_list = document.getElementById("bell-list");
+let bell_list_el = document.getElementById("bell-list");
 
-function bell_in(bell_list, bell) {
+function bell_in(bell_list, bell_name) {
     for (let i of bell_list) {
-        if (i.name === bell.name) {
+        if (i.name === bell_name) {
             return true
         }
     }
@@ -33,14 +37,14 @@ function toggle_controls() {
     document.getElementById("editexisting").classList.toggle("d-hide");
 }
 
-bell_list.onclick = function (e) { // refactor to only take name - rest should be stored in data
+bell_list_el.onclick = function (e) { // refactor to only take name - rest should be stored in data
     if (e.target.nodeName === "TD") {
-        let bell = new Bell(e.target.parentNode);
+        let bell_name = e.target.parentNode.childNodes[1].innerHTML;
         e.target.parentNode.classList.toggle('active');
-        if (bell_in(selected_bells, bell)) {
-            selected_bells = selected_bells.filter(function (item) {
-                return item.name !== bell.name;
-            });
+        let index = selected_bells.indexOf(bell_name);
+        if (index !== -1) {
+            // Bell already selected
+            selected_bells.splice(index, 1);
             if (selected_bells.length === 0) {
                 toggle_controls();
             }
@@ -48,30 +52,37 @@ bell_list.onclick = function (e) { // refactor to only take name - rest should b
             if (selected_bells.length === 0) {
                 toggle_controls();
             }
-            selected_bells.push(bell)
+            selected_bells.push(bell_name)
         }
     }
 };
 
 function render_bells(data) {
-    bell_list.innerHTML = "";
+    bell_list_el.innerHTML = "";
     for (let bell of data) {
-        bell_list += bell.renderHTML();
+        bell_list_el += bell.renderHTML();
     }
 }
 
 function add_bells() {
     // Get data from form
-    document.querySelector("#addnew#bell-name")
-    // Create bell and insert into list
+    let time = document.querySelector("#addnew #bell-time");
+    let name = document.querySelector("#addnew #bell-name");
+    let file = document.querySelector("#addnew #bell-file");
+
+    all_bells.push(new Bell(time, name, file));
+
     // Clear form and render data
+    document.querySelector("#addnew #bell-time").value;
+    document.querySelector("#addnew #bell-name");
+    document.querySelector("#addnew #bell-file");
 }
 
 function delete_bells() {
     // Remove selected bells from all_bells
-    for (let bell of selected_bells) {
+    for (let bell_name of selected_bells) {
         all_bells = all_bells.filter(function (item) {
-            return item.name !== bell.name;
+            return item.name !== bell_name;
         });
     }
 
