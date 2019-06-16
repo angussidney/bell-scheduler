9707c6706362bafcc50b35ad078cd7fc2a3da49e
angussidney <4848802+angussidney@users.noreply.github.com>
2019-06-16 22:06:35 +1000

    Finished bell management panel
    - Added 'add' and 'edit' functionality
    - Bells should now be inserted in sorted order
    - Add filler message to indicate no bells
    - Add cleave.js dependency to restrict user input for times
    - Hide days checkboxes when 'apply to' checkbox is unchecked

--- bell-server/static/main.js
+++ bell-server/static/main.js
@@ -1,4 +1,6 @@
-// TODO: Use actual date obj instead of string
+// TODO: Use actual date obj instead of string - or not?
+// TODO: use forEach instead of for-of
+// TODO: Separate javascript into multiple files
 
 let all_bells = [], // List of bell objects
     selected_bells = []; // List of names
@@ -23,15 +25,6 @@ function get_bell(bell_name) {
 
 let bell_list_el = document.getElementById("bell-list");
 
-function bell_in(bell_list, bell_name) {
-    for (let i of bell_list) {
-        if (i.name === bell_name) {
-            return true
-        }
-    }
-    return false
-}
-
 function toggle_controls() {
     document.getElementById("addnew").classList.toggle("d-hide");
     document.getElementById("editexisting").classList.toggle("d-hide");
@@ -47,10 +40,14 @@ bell_list_el.onclick = function (e) { // refactor to only take name - rest shoul
             selected_bells.splice(index, 1);
             if (selected_bells.length === 0) {
                 toggle_controls();
+            } else if (selected_bells.length === 1) {
+                document.getElementById("edit").classList.toggle("d-hide");
             }
         } else {
             if (selected_bells.length === 0) {
                 toggle_controls();
+            } else if (selected_bells.length === 1) {
+                document.getElementById("edit").classList.toggle("d-hide");
             }
             selected_bells.push(bell_name)
         }
@@ -58,24 +55,68 @@ bell_list_el.onclick = function (e) { // refactor to only take name - rest shoul
 };
 
 function render_bells(data) {
-    bell_list_el.innerHTML = "";
-    for (let bell of data) {
-        bell_list_el += bell.renderHTML();
+    if (data.length > 0) {
+        bell_list_el.innerHTML = "";
+        for (let bell of data) {
+            bell_list_el.innerHTML += bell.renderHTML();
+        }
+    } else if (document.querySelector("#addnew #bell-name").value === "") {
+        // Table is actually empty
+        bell_list_el.innerHTML = "<tr><td colspan='3' style='text-align: center'><em>There are currently no bells scheduled.</em></td></tr>"
+    } else {
+        bell_list_el.innerHTML = "<tr><td colspan='3' style='text-align: center'><em>You are currently editing the only bell.</em></td></tr>"
     }
 }
 
 function add_bells() {
     // Get data from form
-    let time = document.querySelector("#addnew #bell-time");
-    let name = document.querySelector("#addnew #bell-name");
-    let file = document.querySelector("#addnew #bell-file");
+    let time = document.querySelector("#addnew #bell-time").value;
+    let name = document.querySelector("#addnew #bell-name").value;
+    let file = document.querySelector("#addnew #bell-file").value;
 
-    all_bells.push(new Bell(time, name, file));
+    // TODO: Disallow same name/same time, or empty fields
+    // time.setCustomValidity("No two bells can be scheduled for the same time");
+    // name.setCustomValidity("No two bells can have the same name");
+
+    // Sort into list
+    let new_bell = new Bell(time, name, file);
+    let inserted = false;
+    for (let i = 0; i < all_bells.length; i++) {
+        if (all_bells[i].time > new_bell.time) {
+            all_bells = all_bells.slice(0, i).concat(new_bell, all_bells.slice(i));
+            inserted = true;
+            break;
+        }
+    }
+    if (!inserted) {
+        all_bells.push(new_bell);
+    }
 
     // Clear form and render data
-    document.querySelector("#addnew #bell-time").value;
-    document.querySelector("#addnew #bell-name");
-    document.querySelector("#addnew #bell-file");
+    document.querySelector("#addnew #bell-time").value = "";
+    document.querySelector("#addnew #bell-name").value = "";
+    document.querySelector("#addnew #bell-file").selectedIndex = 0;
+    document.getElementById("add").innerHTML = "Add";
+    render_bells(all_bells);
+}
+
+function edit_bells() {
+    // Prefill the 'add new' form
+    let bell = get_bell(selected_bells[0]);
+    document.querySelector("#addnew #bell-time").value = bell.time;
+    document.querySelector("#addnew #bell-name").value = bell.name;
+    document.querySelector("#addnew #bell-file").value = bell.filepath;
+
+    // Remove the bell from the table
+    all_bells = all_bells.filter(function (item) {
+        return item.name !== bell.name;
+    });
+
+    // Re-render form for editing
+    render_bells(all_bells);
+    toggle_controls();
+    document.getElementById("add").innerHTML = "Save";
+    selected_bells = [];
 }
 
 function delete_bells() {
@@ -93,3 +134,17 @@ function delete_bells() {
     render_bells(all_bells);
     toggle_controls();
 }
+
+
+new Cleave('#bell-time', {
+    time: true,
+    timePattern: ['h', 'm']
+});
+
+document.getElementById("apply").onclick = function (ev) {
+    let checked = document.getElementById("apply").checked;
+    document.querySelectorAll("input[name='apply_to']").forEach(function (el) {
+        el.disabled = !checked;
+    });
+};
+
